c4112685bb64c446e88c2fab353872fecd32fbee
AliGaxiola <ali.gaxiola@uabc.edu.mx>
2024-06-06 18:33:30 -0700

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Task 1",
+    description: "first task",
+  },
+  {
+    id: 1,
+    title: "Task 2",
+    description: "second task",
+  },
+  {
+    id: 2,
+    title: "Task 3",
+    description: "third task",
+  },
+];
